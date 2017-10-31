617646f5fc87aa736a98ac736c6c9e64d4f2c403
Vinicius Guedes <vinicius.guedes@parpe.com>
2017-10-31 21:26:50 -0200

    Add information about position to results
    Possible get information about domain according to results returned
    
    Tests adding information about domain
    
    Corrects test that checks domain information
    
    Corrects verification of domain on results list
    
    Corrects comparision with domain that will have information got

--- app/Controllers/AppController.php
+++ app/Controllers/AppController.php
@@ -31,12 +31,22 @@ class AppController
                 'message' => ['You must defined a term parameter']
             ], JsonResponse::HTTP_BAD_REQUEST);
     
-        $results = SERPGoogle::get($request->get('term'));
-    
-        return new JsonResponse([
-            'status' => true,
-            'results' => $results
-        ], Response::HTTP_OK);
+	$results = [
+	    'status' => true,
+	    'results' => SERPGoogle::get($request->get('term'))
+    	];
+	if (!is_null($request->get('domain'))) {
+	    foreach ($results['results'] as $result) {
+		if (!isset($result['info']) && strpos($result['url'], $request->get('domain')) >= 0) {
+		    $results['info'] = [
+			'domain' => $request->get('domain'),
+			'position' => $result['position']
+		    ];
+		}
+	    }
+	}
+
+        return new JsonResponse($results, Response::HTTP_OK);
     }
 
 }
--- app/Services/SERPGoogle.php
+++ app/Services/SERPGoogle.php
@@ -35,7 +35,7 @@ class SERPGoogle
         preg_match_all('/\<div\sclass\=\"g\"(.*)/', $googleHtmlContent, $googleResultsMatched);
 
         $results = [];
-        foreach ($googleResultsMatched[1] as $matchedResult) {
+        foreach ($googleResultsMatched[1] as $index => $matchedResult) {
             preg_match('/\<h3\sclass\=\"r\"\>\<a\shref\=\"(.*?)\>(.*)\<\/a\>\<\/h3\>/', $matchedResult, $matchedTitle);
             preg_match('/\<cite\>(.*)\<\/cite\>/', $matchedResult, $matchedUrl);
             preg_match('/\<span\sclass\=\"st\"\>(.*)/', $matchedResult, $matchedDescription);
@@ -43,7 +43,12 @@ class SERPGoogle
             $results[] = [
                 'url' => !empty($matchedUrl) ? strip_tags(utf8_encode($matchedUrl[1])) : null,
                 'title' => !empty($matchedTitle) ? strip_tags(utf8_encode($matchedTitle[2])) : null,
-                'description' => !empty($matchedDescription) ? strip_tags(utf8_encode($matchedDescription[1])) : null
+                'description' => !empty($matchedDescription) ? strip_tags(utf8_encode($matchedDescription[1])) : null,
+                'position' => [
+                    'page' => floor(($index) / 10) + 1,
+                    'position' => substr((string) $index, -1) + 1,
+                    'general' => $index
+                ]
             ];
         }
 
