139d5307b1dbc66fa84b955ead7f08f4bc3fdfe9
Vinicius Guedes <vinicius.guedes@parpe.com>
2017-11-01 15:40:09 -0200

    Project structure correction

--- app/Services/SERP/GoogleService.php
+++ app/Services/SERP/GoogleService.php
@@ -3,34 +3,14 @@
 namespace SERPer\Services\SERP;
 
 
-use GuzzleHttp\Client;
-
-
-class GoogleService implements ISERPService
+class GoogleService extends SERPService
 {
+    
+    protected static $searchUrl = 'https://www.google.com/search?q=_TERM_&num=_NUM_&gws_rd=cr&dcr=0';
 
-    private static $googleSearchUrl = 'https://www.google.com/search?q=_TERM_&num=_NUM_&gws_rd=cr&dcr=0';
-    private static $numberOfResults = 100;
-    private static $userAgents = [
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0',
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
-        'Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)',
-        'Googlebot/2.1 (+http://www.google.com/bot.html)'
-    ];
+    protected static $numberOfResults = 5;
 
-    public static function get($term)
-    {
-        $googleHtmlContent = self::makeRequest($term);
-        if (is_null($googleHtmlContent))
-            return [];
-
-        $results = self::parseResults($googleHtmlContent);
-        
-        return $results;
-    }
-
-    public static function parseResults($googleHtmlContent)
+    public static function parseResults(string $googleHtmlContent):array
     {
         preg_match_all('/\<div\sclass\=\"g\"(.*)/', $googleHtmlContent, $googleResultsMatched);
 
@@ -53,29 +33,6 @@ class GoogleService implements ISERPService
         }
 
         return $results;
-    }
-
-    public static function makeRequest($term)
-    {
-        $client = new Client();
-        $response = $client->request('GET', str_replace([
-            '_TERM_', '_NUM_'
-        ], [ $term, self::$numberOfResults ], self::$googleSearchUrl), [
-            'Accept-Encoding' => 'gzip, deflate, br',
-            'User-Agent' => self::getRandomUserAgent(),
-            'timeout' => 5000
-        ]);
-
-        if ($response->getStatusCode() !== 200)
-            return null;
-
-        return (string) $response->getBody();
-    }
-
-    public static function getRandomUserAgent()
-    {
-        $max = count(self::$userAgents) - 1;
-        return self::$userAgents[rand(0, $max)];
     }
 
 }
--- app/Services/SERP/ISERPService.php
+++ app/Services/SERP/ISERPService.php
@@ -6,6 +6,8 @@ namespace SERPer\Services\SERP;
 interface ISERPService
 {
 
-    public static function get($word);
+    public static function get(string $term):array;
+
+    public static function parseResults(string $serpHtmlContent):array;
 
 }
