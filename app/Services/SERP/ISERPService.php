2fcb9bc8cbb260252da4e036da9ba9776c8d6727
Vinicius Guedes <viniciusgued@gmail.com>
2017-11-07 15:26:49 -0200

    Revert "Possible force a region where search should be made (#11)"
    This reverts commit 4c2cb60cefe33b7269f74d0367d057b4c3c2ad26.

--- app/Services/SERP/ISERPService.php
+++ app/Services/SERP/ISERPService.php
@@ -6,10 +6,8 @@ namespace SERPer\Services\SERP;
 interface ISERPService
 {
 
-    public static function get(string $term, string $location = null):array;
+    public static function get(string $term):array;
 
     public static function parseResults(string $serpHtmlContent):array;
-
-    public static function parseLocation(string $location):string;
 
 }
